3f03dfc57bfe27a845f1e401d89d301652a06280
asmaa zaid <asmaazaid921@gmail.com>
2026-07-02 03:01:17 +0300

    Save my lastestAPI controller and dashbour changes

---
+++ Shefaa/DTOs/dashboard/DBcounter.cs
@@ -0,0 +1,10 @@
+﻿namespace Shefaa.DTOs.dashboard
+{
+    public class DBcounter
+    {
+        public int TotalUsers { get; set; }
+        public int  TotalDoctors { get; set; }
+        public int TotalAppointments { get; set; }
+        public int TotalOrganizations { get; set; }
+    }
+}
