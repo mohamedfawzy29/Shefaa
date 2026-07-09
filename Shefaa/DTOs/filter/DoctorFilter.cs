dbc4dde1e88c0c2f6613f08c8e5a36b976c1844c
asmaa zaid <asmaazaid921@gmail.com>
2026-07-09 05:24:26 +0300

    Add review and appointment DTOs

---
+++ Shefaa/DTOs/filter/DoctorFilter.cs
@@ -0,0 +1,11 @@
+﻿namespace Shefaa.DTOs.filter
+{
+    public class DoctorFilter
+    {
+        public string? SearchQuery { get; set; }      
+        public Guid? SpecializationId { get; set; }   
+          
+        public string? OrderBy { get; set; }         
+    }
+
+}
